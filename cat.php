ca9814b8c3096bfe9fde18ff5316d5d9e0a2b9f2
lill74 <a0103932@naver.com>
2018-09-26 00:06:31 +0900

    Update cat.php

--- cat.php
+++ cat.php
@@ -8,21 +8,32 @@ class CAT {
     }
 
     function CreateToken($payload, $salt = null) {
-        $data = "";
-
         $data = base64_encode($payload) . "." . hash_hmac("sha256", base64_encode($payload), $this -> defsalt . $salt);
 
         return($data);
     }
 
-    function VerifyToken($data, $salt = null) {
+    function VerifyToken($data, $acl, $ref = null, $salt = null) {
         $str = explode(".", $data);
 
-        if(hash_hmac("sha256", $str[0], $this -> defsalt . $salt) == $str[1]) {
-            return true;
+        if(count($str) <= 1) {
+            return false;
         }
-        else
-        {
+
+        if(hash_hmac("sha256", $str[0], $this -> defsalt . $salt) == $str[1]) {
+
+            $jsondcode = json_decode(base64_decode($str[0]));
+
+            if(intval($jsondcode -> {"expire"}) > time() && $acl == $jsondcode -> {"acl"} && $jsondcode -> {"ipadr"} == $_SERVER["REMOTE_ADDR"]) {
+                    if(isset($ref)) {
+                        if($_SERVER['HTTP_REFERER'] == $ref)
+                        {
+                            return true;
+                        }
+                        return false;
+                    }
+                    return true;
+            }
             return false;
         }
     }
